379ee4d2f64be31b00179ff0fb2c8c90f682cca7
Matiran <maayan.tiran@gmail.com>
2025-02-04 18:54:39 +0200

    remove hardcoded mongo uri

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CodeBlockModule } from './code-block/code-block.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config'; // Add this line
 
 @Module({
   imports: [
-    MongooseModule.forRoot("mongodb://localhost:27017/coding-web-app"),
+    ConfigModule.forRoot(), // Load environment variable
+    MongooseModule.forRoot(process.env.MONGODB_URI),
     CodeBlockModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
+// mongodb+srv://maayan_tiran:<db_password>@online-coding-web-app.lotev.mongodb.net/?retryWrites=true&w=majority&appName=Online-Coding-Web-App"
