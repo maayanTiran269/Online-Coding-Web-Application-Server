3eaa7393a7b44f895c9670b8fe7742869858bc6a
Matiran <maayan.tiran@gmail.com>
2025-02-05 14:27:44 +0200

    change url

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,11 @@ import { ConfigModule } from '@nestjs/config'; // Add this line
 @Module({
   imports: [
     ConfigModule.forRoot(), // Load environment variable
-    // MongooseModule.forRoot(process.env.MONGODB_URI),
-    MongooseModule.forRoot("mongodb://localhost:27017/coding-web-app"),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+    // MongooseModule.forRoot("mongodb://localhost:27017/coding-web-app"),
 
     CodeBlockModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
-// mongodb+srv://maayan_tiran:<db_password>@online-coding-web-app.lotev.mongodb.net/?retryWrites=true&w=majority&appName=Online-Coding-Web-App"
